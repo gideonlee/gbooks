e65e11905ed6d53b1ea9b59c6e5390df9b832b14
Gideon Lee <Gideonxlee@gmail.com>
2022-12-30 17:47:28 -0800

    no results found message

--- index.js
+++ index.js
@@ -22,34 +22,39 @@ program
       bookSearch({query: query})
       .then((res) => {
         if (res.success) {
-          // Prompt user to add book based on query results. Allows for none of these.  
-          inquirer.prompt([
-            {
-              type: 'list',
-              name: 'book',
-              message: `Add a book to your Reading List:`,
-              choices: res.books.concat(['None of these.'])
-            }
-          ])
-          .then(answer => {
-            // If book is selected, add book to list if it's not already in the list
-            if (answer.book !== 'None of these.') {
-              // Get the reading list or create an empty list
-              let readingList = config.get('readingList') || []
-  
-              // Check if book is already in list 
-              const isInList = readingList.includes(answer.book)
-  
-              // Add book if it isn't in reading list
-              if (!isInList) {
-                readingList.push(answer.book)
-                config.set('readingList', readingList)
-                console.log(`Successfully added ${answer.book}`)
-              } else {
-                console.log(`${answer.book} is already in your reading list.`)
+          if (res.books.length === 0) {
+            // Query returned zero results.
+            console.log('No results found.')
+          } else {
+            // Prompt user to add book based on query results. Allows for none of these.  
+            inquirer.prompt([
+              {
+                type: 'list',
+                name: 'book',
+                message: `Add a book to your Reading List:`,
+                choices: res.books.concat(['None of these.'])
               }
-            }
-          })
+            ])
+            .then(answer => {
+              // If book is selected, add book to list if it's not already in the list
+              if (answer.book !== 'None of these.') {
+                // Get the reading list or create an empty list
+                let readingList = config.get('readingList') || []
+    
+                // Check if book is already in list 
+                const isInList = readingList.includes(answer.book)
+    
+                // Add book if it isn't in reading list
+                if (!isInList) {
+                  readingList.push(answer.book)
+                  config.set('readingList', readingList)
+                  console.log(`Successfully added ${answer.book}`)
+                } else {
+                  console.log(`${answer.book} is already in your reading list.`)
+                }
+              }
+            })
+          }
         } else {
           // bookSearch error response.
           console.log(res.message)
